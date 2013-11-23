ad18f14f8fd53e5acb4859859024941671aec683
Nicolas <rudas.n@gmail.com>
2013-11-23 05:52:51 +0200

    settings

--- manage.py
+++ manage.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+import getopt
 
 if __name__ == "__main__":
     settings = os.environ.get("DJANGO_SETTINGS_MODULE")
 
-    if not DJANGO_SETTINGS_MODULE:
-        print 'No settings found'
+    if not settings:
+        for arg in sys.argv:
+            settings, args = getopt.getopt([arg], '', 'settings=')
+            if settings:
+                break
+        if not settings:
+            print 'No settings found. You need to specify the settings file by passing  --settings={{ project_name }}.settings.development or set a global "DJANGO_SETTINGS_MODULE" variable with the name of your settings file, eg. {{ project_name }}.settings.development'
 
+    if settings:
+        from django.core.management import execute_from_command_line
 
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv)
